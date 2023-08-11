a4d02668c1509722a26ce8d9a4ce665151944de6
loshchenko <elena.loshchenko@sbermegamarket.ru>
2023-08-12 00:39:42 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,56 @@
-package hw02unpackstring
+package main
 
 import (
+	"bytes"
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isDigit(char string) (int, bool) {
+	val, err := strconv.Atoi(char)
+	return val, err != nil
+}
+
+func removeLastLetterFromBuffer(buffer *bytes.Buffer) {
+	strFromBuffer := buffer.String()
+	currStr := strFromBuffer[0 : len(strFromBuffer)-1]
+	var bufferTemp bytes.Buffer
+	bufferTemp.WriteString(currStr)
+	*buffer = bufferTemp
+}
+
+func Unpack(str string) (string, error) {
+	if len(str) == 0 {
+		return "", nil
+	}
+
+	if _, isNan := isDigit(string(str[0])); !isNan {
+		return "", ErrInvalidString
+	}
+
+	var buffer bytes.Buffer
+	for index, char := range str {
+		letter := string(char)
+
+		if counter, isNan := isDigit(letter); isNan {
+			buffer.WriteString(letter)
+		} else {
+			lastLetter := string(str[index-1])
+
+			if _, isNan := isDigit(lastLetter); !isNan {
+				return "", ErrInvalidString
+			}
+
+			if counter == 0 {
+				removeLastLetterFromBuffer(&buffer)
+			} else {
+				buffer.WriteString(strings.Repeat(lastLetter, counter-1))
+			}
+		}
+	}
+
+	return buffer.String(), nil
 }
